14d3a0612aa2f44a4662768c42e343b3ad1f3b14
ramy handousa <ramyahmedhandousa@gmail.com>
2022-04-21 14:28:04 +0200

    Update ProductRateController.php

--- app/Http/Controllers/Api/Products/ProductRateController.php
+++ app/Http/Controllers/Api/Products/ProductRateController.php
@@ -15,7 +15,7 @@ class ProductRateController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware('auth:api')->except('index');
     }
 
     public function index(ValidExitProduct $request)
