f661396a69261293f197950c92f4c4ab6f1f56e6
ramy handousa <ramyahmedhandousa@gmail.com>
2022-04-21 14:34:05 +0200

    Update ProductRateController.php

--- app/Http/Controllers/Api/Products/ProductRateController.php
+++ app/Http/Controllers/Api/Products/ProductRateController.php
@@ -10,6 +10,7 @@ use App\Http\Resources\Products\RatingProductResource;
 use App\Models\ProductRate;
 use App\Support\Facade\Responder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductRateController extends Controller
 {
@@ -22,7 +23,20 @@ class ProductRateController extends Controller
     {
         $rating = ProductRate::whereProductId($request->product_id)->whereNull('parent_id')->with('user','children')->get();
 
-        return Responder::success(RatingProductResource::collection($rating));
+        $countRatingProducts = ProductRate::where('product_id',$request->product_id)->select('product_id',
+            DB::raw('count(*) as count'),
+            DB::raw('round(AVG(rate),1) as avg_rate'),
+            DB::raw('count(IF(rate = 0,1,NULL)) as  one'),
+            DB::raw('count(IF(rate = 2,2.5,NULL)) as two'),
+            DB::raw('count(IF(rate = 3,3.5,NULL)) as three'),
+            DB::raw('count(IF(rate = 4,4.5,NULL)) as four'),
+            DB::raw('count(IF(rate = 5,5,NULL))  as five')
+        )->groupBy('product_id')->first();
+
+        return Responder::success([
+            'info' => $countRatingProducts,
+            'data' => RatingProductResource::collection($rating)
+        ]);
     }
 
     public function show($id)
